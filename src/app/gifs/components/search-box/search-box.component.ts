8c090b8c428fd52d143398ce3ec482527a6a1cab
Diego La Faye <lafayediego@gmail.com.com>
2024-01-25 17:17:58 -0400

    added validations to the gifs service

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
 })
+
+//Datos importantes: En html para poner una referencia y asi no llamar si es que no fuese necesario el formModule de angular  se puede referencia un input con el simbolo de # y asi obtener el value:
+// (keyup.enter)="searchTag(txtTagInput.value)"  ==> Datazo en angular puedes utilizar el keyup.enter para que ejecute una accion cuando se apriete enter
+// Ese txtTagInput viene siendo la referencia que se coloca en el html
 export class SearchBoxComponent {
-  constructor() { }
+  constructor(private gifsService:GifsService) { }
+  // esta es la manera sin  utilizar el view Child
+  // searchTag(newTag:string){
+    //   console.log({newTag});
+
+    // }
+    //Esta es la manera con viewChild
+    @ViewChild('txtTagInput')
+    public tagInput!:ElementRef<HTMLInputElement>;
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value ='';
+
+    console.log({newTag});
+
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent{
+  constructor(private gifsService:GifsService){}
+  data:string[]=[];
 
+get tags(){
+  return this.gifsService.tagsHistory;
+}
 }
