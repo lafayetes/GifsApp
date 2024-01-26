29e5c15be5a410f3f1dbb88cfd2dac5fe2b3dd6f
Diego La Faye <lafayediego@gmail.com.com>
2024-01-26 12:13:25 -0400

    added data persistency

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,10 @@ export class GifsService {
   private apiKey: string = 'SjYXXKCb7AtsUxgmOjDGu2wsDC8oFUqg';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('GIFS loaded');
+   }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -26,7 +29,23 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+
   }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history',JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history'))return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')! );
+    if(this._tagsHistory.length===0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
+
   // Esta es la forma rudimentaria de hacer una peticion
   //  async searchTag(tag:string) : Promise<void> {
   //     if (tag.length === 0) return;
